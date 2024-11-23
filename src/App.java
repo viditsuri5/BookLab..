43238ab52c16bc4c884937bda28a916675bfb688
viditsuri5 <viditsuri5@gmail.com>
2024-11-23 06:07:35 +0000

    Nothing

--- src/App.java
+++ src/App.java
@@ -1,24 +1,29 @@
-import java.util.ArrayList;
+public class BookTranslator {
+    public static void main(String[] args){
+        UtilityTests.runTests();
+        translateAndSave(
+            "Romeo & Juliet",
+            "https://www.gutenberg.org/cache/epub/1513/pg1513.txt",
+            "Romeo_Juliet_PigLatinOutput.txt"
+         );
+        translateAndSave(
+            "Jane Eyre",
+            "https://www.gutenberg.org/cache/epub/1260/pg1260.txt",
+            "JaneEyre_PigLatinOutput.txt"
+        );
+    }
 
-public class App {
-    public static void main(String[] args)
-    {
-        // Variable to store the songs
-        ArrayList<Song> songs;
+    private static void translateAndSave(String title, String fileUrl, String outputFile) {
+        DigitalBook sourceBook = new DigitalBook();
+        sourceBook.fetchFromWeb(title, fileUrl);
+        System.out.println("Extracted Content from (" + title + "):");
+        sourceBook.previewLines(1, 4);
 
-        // import the songs from the CSV using the SongImporter
-        songs = SongImporter.importSongsFromCSV("songs.csv");
+        DigitalBook pigLatinBook = PigLatinProcessor.process(sourceBook);
+        System.out.println("Pig Latin Translation for (" + title + "):");
+        pigLatinBook.exportToFile(outputFile);
+        System.out.println("Translation successfully saved at: " + outputFile);
 
-        // Loop across all the songs imported
-        for (int i=0; i<songs.size(); i++)
-        {
-            // Get the current song matching the loop index i
-            Song s = songs.get(i);
-
-            // Example - print the song
-            // You should add code to compute song statistics here.
-            System.out.println("Artist:" + s.getArtist());
-        }       
     }
 }
 
